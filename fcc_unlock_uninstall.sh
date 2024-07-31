e1ec4a90b59c495665aee7e29968539fa1b3352b
Atlas Yu <85104110+pseudocc@users.noreply.github.com>
2024-07-31 14:09:18 +0800

    feat(ModemManager.service): use drop-in override (#18)
    * feat(ModemManager.service): use drop-in override
    
    Instead of modifying /usr/lib/systemd/system/ModemManager.service
    directly. Use a drop-in configuration to override the `ExecStart`. So,
    even if users update the ModemManager with their package manager, the
    override will not be affect, which means they don't need to rerun the
    fcc_unlock_setup.sh.
    
    A wrapper is introduced to check users' ModemManager's support scope at
    runtime instead of installation time. So users can free to upgrade or
    downgrade ModemManager without encounter the `unknown option` errors.
    
    * fix(uninstall): remove the drop-in file

--- fcc_unlock_uninstall.sh
+++ fcc_unlock_uninstall.sh
@@ -68,33 +68,14 @@ then
 fi
 sudo systemctl daemon-reload
 
-## modify ModemManager.service to delete "--test-low-power-suspend-resume " and " --test-quick-suspend-resume"
-SERVICE_FILE="/lib/systemd/system/ModemManager.service"
-STRING_LOW_POWER=" --test-low-power-suspend-resume"
-STRING_QUICK_SUSPEND=" --test-quick-suspend-resume"
-Rplus_check=$(lspci -d :7560)
-FM350_check=$(lspci -d :4d75)
-RM520_check=$(lspci -d :1007)
-EM160R_check=$(lspci -d :100d)
-CURRENT_CONFIG=$(cat "$SERVICE_FILE")
+PACKAGE_DIR=$(dirname "$0")
+source "$PACKAGE_DIR"/suspend-fix/install.sh
 
-restart_mm_service=false
-
-if [ -n "$Rplus_check" ] || [ -n "$FM350_check" ] || [ -n "$RM520_check" ] || [ -n "$EM160R_check" ]; then
-	if grep -q 'test-quick-suspend-resume' <<< "$CURRENT_CONFIG";then
-		sudo sed -i "s/${STRING_QUICK_SUSPEND}//g" ${SERVICE_FILE}
-		restart_mm_service=true
-	fi
-	if grep -q 'test-low-power-suspend-resume' <<< "$CURRENT_CONFIG";then
-		sudo sed -i "s/${STRING_LOW_POWER}//g" ${SERVICE_FILE}
-		restart_mm_service=true
-	fi
-
-	if [ "$restart_mm_service" == "true" ]
-	then
-		sudo systemctl daemon-reload
-		sudo systemctl restart ModemManager
-	fi
+# Delete the drop-in file if it exists
+if [ -f "$MM_DROPIN_DIR/$MM_DROPIN_FILENAME" ]; then
+    sudo rm -f "$MM_DROPIN_DIR/$MM_DROPIN_FILENAME"
+    sudo systemctl daemon-reload
+    sudo systemctl restart ModemManager.service
 fi
 
 echo "fcc unlock package is uninstalled"
---
+++ suspend-fix/install.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+# vim:ts=4:noet
+
+MM_DROPIN_DIR=/etc/systemd/system/ModemManager.service.d
+MM_DROPIN_FILENAME="apply-test-option.conf"
+MM_SUSPEND_FIX_DIR=$(dirname "${BASH_SOURCE[0]}")
+FCC_LENOVO_DIR=/opt/fcc_lenovo
+
+# Only when certain PCI IDs are found will the drop-in be installed
+MM_PIDS=(
+	"7560" # Rplus
+	"4d75" # FM350
+	"1007" # RM520
+	"100d" # EM160R
+)
+
+. "$MM_SUSPEND_FIX_DIR/mm-wrapper.sh"
+
+precheck_test_options() {
+	local fully_supported=true
+	for topt in "${MM_TEST_OPTIONS[@]}"; do
+		if ! support_test_option "$topt"; then
+			fully_supported=false
+		fi
+	done
+
+	# support_test_option will inform the user of any missing test options
+	if ! $fully_supported; then
+		>&2 echo "ModemManager does not support all test options"
+		>&2 echo "Please update your ModemManager"
+		exit 1
+	fi
+}
+
+install_dropin() {
+	precheck_test_options
+
+	if [ ! -d "$MM_DROPIN_DIR" ]; then
+		echo "Creating drop-in directory $MM_DROPIN_DIR"
+		sudo mkdir -p "$MM_DROPIN_DIR"
+	fi
+
+	sudo cp -vf "$MM_SUSPEND_FIX_DIR/$MM_DROPIN_FILENAME" "$MM_DROPIN_DIR"
+	sudo mkdir -p "$FCC_LENOVO_DIR/suspend-fix"
+	sudo cp -vf "$MM_SUSPEND_FIX_DIR/mm-wrapper.sh" "$FCC_LENOVO_DIR/suspend-fix"
+	sudo chmod ugo+x "$FCC_LENOVO_DIR/suspend-fix/mm-wrapper.sh"
+	
+	sudo systemctl daemon-reload
+	sudo systemctl restart ModemManager.service
+}
+
+if [ "${BASH_SOURCE[0]}" = "$0" ]; then
+	for pid in "${MM_PIDS[@]}"; do
+		if lspci -d ":$pid" &>/dev/null; then
+			echo "Found PCI ID $pid, installing the ModemManager drop-in override"
+			install_dropin
+			exit
+		fi
+	done
+
+	echo "Modem on this system does not need this fix"
+fi
