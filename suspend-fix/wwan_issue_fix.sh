f3f86becb1f0cba0d30cbdfb9727a2913279053e
Nitin Joshi <nitjoshi@gmail.com>
2026-05-12 16:26:14 +0900

    [WWAN] Added suspend fix for SDX61 Modem

--- suspend-fix/wwan_issue_fix.sh
+++ suspend-fix/wwan_issue_fix.sh
@@ -3,17 +3,17 @@
 ## modify ModemManager.service to add "--test-low-power-suspend-resume " and " --test-quick-suspend-resume"
 SERVICE_FILE="/lib/systemd/system/ModemManager.service"
 STRING_LOW_POWER=" --test-low-power-suspend-resume"
-STRING_QUICK_SUSPEND=" --test-quick-suspend-resume"
 Rplus_check=$(lspci -d :7560)
 FM350_check=$(lspci -d :4d75)
 RM520_check=$(lspci -d :1007)
 EM160R_check=$(lspci -d :100d)
+SDX61_check=$(lspci -d :0308)
 CURRENT_CONFIG=$(cat "$SERVICE_FILE")
 
 restart_mm_service=false
 function version_ge() { test "$(echo "$@" | tr " " "\n" | sort -rV | head -n 1)" == "$1"; }
 
-if [ -n "$Rplus_check" ] || [ -n "$FM350_check" ] || [ -n "$RM520_check" ] || [ -n "$EM160R_check" ]; then
+if [ -n "$Rplus_check" ] || [ -n "$FM350_check" ] || [ -n "$RM520_check" ] || [ -n "$EM160R_check" ] || [ -n "$SDX61_check" ]; then
 	curmmver=$(mmcli -V)
 	first_line=${curmmver%%\n*}
 	curmmvernum=$(echo $first_line | cut -d " " -f2)
@@ -35,5 +35,5 @@ if [ -n "$Rplus_check" ] || [ -n "$FM350_check" ] || [ -n "$RM520_check" ] || [ 
 		echo "Fix supports ModemManager version greater than 1.23.2 or later only."
 	fi
 else
-	echo "Issue Fix is only applicable for Fibocom L860-GL-16/FM350 and Quectel EM160R-GL/RM520N-GL WWAN module."
+	echo "Issue Fix is only applicable for Fibocom L860-GL-16/FM350 ,Quectel EM160R-GL/RM520N-GL and Foxconn SDX61 WWAN module."
 fi
